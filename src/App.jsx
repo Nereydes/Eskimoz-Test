4eeadfae407d3bc12e3c9ade49a03b522bfa02da
Krysalizys <krys@MacBook-Pro-de-Krysalizys.local>
2025-02-11 12:25:43 +0100

    User card design

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
 	return (
 		<>
 			<div>
-				<h1 className="text-red-500">Coucou</h1>
 				<GridCard users={sampleData} />
 			</div>
 		</>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,17 @@
 const Card = ({ user }) => {
 	return (
-		<div>
-			<h2>{user.name}</h2>
+		<div
+			className="group w-full min-[445px]:w-40 min-[445px]:hover:w-60 2xl:w-65 2xl:hover:w-80 hover:shadow-lg hover:shadow-indigo-500/50 aspect-3/4 rounded-xl bg-center bg-contain transition-all flex items-end overflow-hidden"
+			style={{
+				backgroundImage: `url('./assets/images/${user.image}`,
+			}}
+		>
+			<div className="w-full h-1/4 bg-linear-to-tr from-blue-700/80 via-purple-700/80 to-indigo-900/80 p-2.5 grid grid-cols-2 grid-rows-2 group-hover:text-base text-lg min-[445px]:text-xs 2xl:text-xl">
+				<span className="font-bold">{user.name}</span>
+				<span>{user.age} y.o.</span>
+				<span className="truncate">{user.job}</span>
+				<span className="truncate">{user.location}</span>
+			</div>
 		</div>
 	);
 };
