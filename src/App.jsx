a5f8258d79eb56a2745e33c7b892b6bd599eeca4
Krysalizys <krys@MacBook-Pro-de-Krysalizys.local>
2025-02-11 11:10:44 +0100

    Components structure with data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import sampleData from "./data/sample.json";
+import GridCard from "./components/GridCard";
 
 function App() {
 	return (
 		<>
 			<div>
 				<h1 className="text-red-500">Coucou</h1>
+				<GridCard users={sampleData} />
 			</div>
 		</>
 	);
---
+++ src/components/GridCard.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+const GridCard = ({ users }) => {
+	return (
+		<div className="flex gap-5 flex-wrap">
+			{users.map((user, index) => (
+				<Card key={`${user.name}-${index}`} user={user} />
+			))}
+		</div>
+	);
+};
+
+export default GridCard;
